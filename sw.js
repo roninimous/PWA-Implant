8e3549e05cd78000997c8db99140658680df632f
roninimous <noukeosombath@gmail.com>
2025-08-25 14:11:51 +1000

    push PWA content template and update readme for installation guides

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'app-name-v1';
+const urlsToCache = [
+  '/',
+  '/login',
+  '/home',
+  '/css/app.css',
+  '/js/app.js',
+  '/logo.png',
+  '/favicon.ico',
+  '/site.webmanifest'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
